5eb575cdabcef3e30d79691b0e0447278779fdbc
isaacvay <isaacyavs@gmail.com>
2025-03-08 12:54:41 +0200

    Update BulletinHeader.tsx

--- app/components/dashbordcomp/bulletin/BulletinHeader.tsx
+++ app/components/dashbordcomp/bulletin/BulletinHeader.tsx
@@ -4,7 +4,8 @@ import React from 'react';
 const BulletinHeader: React.FC = () => (
   <div className="grid grid-cols-[25%_50%_25%] items-center pb-4 mb-4">
     <div className="flex justify-center">
-      <img src="images/flag.png" alt="drapeau" className="w-24 h-16" />
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Flag_of_the_Democratic_Republic_of_the_Congo.svg/1200px-Flag_of_the_Democratic_Republic_of_the_Congo.svg.png"
+ alt="drapeau" className="w-24 h-16" />
     </div>
     <div className="text-center">
       <h1 className="text-xl font-bold">
@@ -15,7 +16,7 @@ const BulletinHeader: React.FC = () => (
       </h2>
     </div>
     <div className="flex justify-center">
-      <img src="images/Armoiries.png" alt="armoirie" className="w-24 h-24" />
+      <img src="https://upload.wikimedia.org/wikipedia/commons/0/05/Coat_of_Arms_Democratic_Republic_of_Congo.png" alt="armoirie" className="w-24 h-24" />
     </div>
   </div>
 );
