fd7c82089965bdb8d0cf05373a2ae921e59bf436
isaacvay <isaacyavs@gmail.com>
2025-03-09 22:21:32 +0200

    Update BulletinFooter.tsx

--- app/components/dashbordcomp/bulletin/BulletinFooter.tsx
+++ app/components/dashbordcomp/bulletin/BulletinFooter.tsx
@@ -13,22 +13,15 @@ const BulletinFooter: React.FC = () => (
         <p>- L’élève double sa classe (1)</p>
         <p>- L’élève a échoué et est orienté vers............... (1)</p>
       </div>
-      <div className="pt-6 pl-20">
+      <div className="pt-4 pl-20">
         <p>Fait à ...................., le…………./………./20……</p>
-        <p className="pt-10 font-bold pl-10">Le Chef d’Etablissement</p>
+        <p className="pt-5 font-bold pl-10">Le Chef d’Etablissement</p>
       </div>
     </div>
     <div className="flex justify-between py-10 px-14 font-bold">
       <p>Signature de l’élève</p>
       <p>Sceau de l’école</p>
       <p>Nom et Signature</p>
-    </div>
-    <div>
-      <p>(1)Biffer la mention inutile</p>
-      <p>
-        Note importante : Le bulletin est sans valeur s’il est raturé ou
-        surchargé
-      </p>
     </div>
   </div>
 );
